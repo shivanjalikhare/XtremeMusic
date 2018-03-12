6f657425d02ebde001fdbb4597f6cae1f94a8dc7
shivanjali.khare@gmail.com <shivanjali.khare@gmail.com>
2018-03-12 13:31:34 -0500

    template code modified with shortcut

--- music/views.py
+++ music/views.py
@@ -1,16 +1,12 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Album 
 
 def index(request):
 	#get all objects from db
 	all_albums = Album.objects.all()
-	template = loader.get_template('music/index.html')
-	context = {
-		'all_albums': all_albums,
-
-	}
-	return HttpResponse(template.render(context, request))
+	context = { 'all_albums': all_albums}
+	return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
 	return HttpResponse("this is detailed view of " + str(album_id) +".")
