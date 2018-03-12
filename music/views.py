3b500c3c4ae9d29c332629207594a281c735e214
shivanjali.khare@gmail.com <shivanjali.khare@gmail.com>
2018-03-12 13:18:13 -0500

    added simple html template for music

--- music/views.py
+++ music/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album 
 
 def index(request):
-	return HttpResponse("this is music app homepage")
+	#get all objects from db
+	all_albums = Album.objects.all()
+	template = loader.get_template('music/index.html')
+	context = {
+		'all_albums': all_albums,
+
+	}
+	return HttpResponse(template.render(context, request))
+
+def detail(request, album_id):
+	return HttpResponse("this is detailed view of " + str(album_id) +".")
