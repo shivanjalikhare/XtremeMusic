2240f72d4ab4f76a266a41bd457b13a3a81fd2b0
shivanjali.khare@gmail.com <shivanjali.khare@gmail.com>
2018-03-19 19:03:38 -0500

    features added-add albums,navbar,UI updated

--- music/urls.py
+++ music/urls.py
@@ -5,8 +5,13 @@ app_name = 'music'
 
 urlpatterns = [
 	# /music/ ~home page for music
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     
     # /music/71/ (album_id)
-    re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    #/music/album/add/
+    re_path(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+   
 ]
--- music/views.py
+++ music/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Album 
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView 
+from .models import Album
 
-def index(request):
-	#get all objects from db
-	all_albums = Album.objects.all()
-	context = { 'all_albums': all_albums}
-	return render(request, 'music/index.html', context)
+class IndexView(generic.ListView):
+	template_name = 'music/index.html'
+	context_object_name = 'all_albums'
 
-def detail(request, album_id):
-	#album = Album.objects.get(pk=album_id)
-	album = get_object_or_404(Album, pk=album_id)
-	return render(request, 'music/detail.html', { 'album': album})
+	def get_queryset(self):
+		return Album.objects.all()
+
+class DetailView(generic.DetailView):
+	model = Album
+	template_name = "music/detail.html"
 	
+		
+class AlbumCreate(CreateView):
+	model = Album
+	fields = ['artist', 'album_title', 'genre', 'album_logo']
