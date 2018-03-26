a04fafee42a2c70e2d64067e4b0efb5fd4ccd03c
shivanjali.khare@gmail.com <shivanjali.khare@gmail.com>
2018-03-26 07:21:12 -0500

    user authenticate and login

--- music/urls.py
+++ music/urls.py
@@ -6,6 +6,8 @@ app_name = 'music'
 urlpatterns = [
 	# /music/ ~home page for music
     path('', views.IndexView.as_view(), name='index'),
+
+    re_path(r'^register/$', views.UserFormView.as_view(), name='register'),
     
     # /music/71/ (album_id)
     re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
@@ -13,5 +15,10 @@ urlpatterns = [
     #/music/album/add/
     re_path(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
+	#/music/album/2/ update
+    re_path(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
    
+    #/music/album/2/ delete
+    re_path(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+
 ]
