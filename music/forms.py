744fed95e5c7ca017def7991578e308d5208ccb3
Abhi <abhimike17@gmail.com>
2018-05-02 22:54:34 -0500

    optimizing code

--- music/forms.py
+++ music/forms.py
@@ -7,3 +7,17 @@ class UserForm(forms.ModelForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password']
+
+		class SongForm(forms.ModelForm):
+
+            class Meta:
+                model = Song
+                fields = ['song_title', 'audio_file']
+
+
+        class UserForm(forms.ModelForm):
+            password = forms.CharField(widget=forms.PasswordInput)
+
+            class Meta:
+                model = User
+                fields = ['username', 'email', 'password']
